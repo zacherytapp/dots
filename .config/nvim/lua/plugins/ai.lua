a91bec22def19b3be5f230a1f5b9011527cbf0c8
Zakk Tapp <zakk@craftsmantech.com>
2023-11-03 13:52:08 -0400

    added ai

---
+++ .config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          accept = false, -- disable built-in keymapping
+        },
+      })
+    end,
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      local home = vim.fn.expand("$HOME")
+      require("chatgpt").setup({
+        api_key_cmd = "gpg --decrypt --quiet ".. home .."/.config/chatgpt.txt.gpg"
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  }
+}
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -16,6 +16,11 @@ return {
 		'rafamadriz/friendly-snippets',
 	},
 	config = function ()
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
 		local cmp = require 'cmp'
 		local luasnip = require 'luasnip'
 
@@ -41,9 +46,13 @@ return {
 				},
 				['<Tab>'] = cmp.mapping(function(fallback)
 					if cmp.visible() then
-						cmp.select_next_item()
+						cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+					elseif require("copilot.suggestion").is_visible() then
+						require("copilot.suggestion").accept()
 					elseif luasnip.expand_or_locally_jumpable() then
 						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
 					else
 						fallback()
 					end
