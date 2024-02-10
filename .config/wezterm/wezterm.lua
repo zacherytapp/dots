560b34df2547c90393866f48643f26cd3a47855d
Zachery Tapp <zacherytapp@gmail.com>
2024-02-10 13:55:51 -0500

    added some new stuff

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,5 +10,39 @@ config.cursor_blink_ease_out = "Constant"
 
 config.color_scheme_dirs = { "/home/zakk/wezterm" }
 config.color_scheme = "Dracula (Official)"
+config.window_background_opacity = 0.95
+
+-- Fonts
+config.font_size = 14
+config.font = wezterm.font({ family = "Fira Code" })
+config.bold_brightens_ansi_colors = true
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font({
+			family = "Maple Mono",
+			weight = "Bold",
+			style = "Italic",
+		}),
+	},
+	{
+		italic = true,
+		intensity = "Half",
+		font = wezterm.font({
+			family = "Maple Mono",
+			weight = "DemiBold",
+			style = "Italic",
+		}),
+	},
+	{
+		italic = true,
+		intensity = "Normal",
+		font = wezterm.font({
+			family = "Maple Mono",
+			style = "Italic",
+		}),
+	},
+}
 
 return config
