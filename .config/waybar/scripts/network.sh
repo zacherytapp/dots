a0f012b0aeb80b4ed2541cd2f2a5d13f8ab2209b
Zakk Tapp <zacherytapp@gmail.com>
2024-07-30 21:58:19 -0400

    removed a bunch of stuff

--- .config/waybar/scripts/network.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-stat=`vnstat -i wlp3s0 -tr 2 --json`
-tx=`echo "$stat" | jq .tx.ratestring | sed 's/"//g ; s/bit/b/g'`
-rx=`echo "$stat" | jq .rx.ratestring | sed 's/"//g ; s/bit/b/g'`
-
-echo "{\"text\": \" $tx   $rx\", \"alt\": \"bc\"}"
