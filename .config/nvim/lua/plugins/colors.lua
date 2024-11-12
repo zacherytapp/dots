797960d3c97c536b54cf923bd6f9aeba9ae57b8a
Zachery Tapp <zacherytapp@gmail.com>
2024-11-12 09:43:56 -0500

    back to gruvbox

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,16 +1,8 @@
 return {
-	{
-		"Mofiqul/dracula.nvim",
-		priority = 1000,
-		config = function()
-			local opts = {
-				transparent_bg = true,
-				overrides = {
-					["@lsp.type.method"] = { fg = "#ff9900" },
-				},
-			}
-			require("dracula").setup(opts)
-			vim.cmd("colorscheme dracula")
-		end,
-	},
+	"eddyekofo94/gruvbox-flat.nvim",
+	priority = 1000,
+	enabled = true,
+	config = function()
+		vim.cmd([[colorscheme gruvbox-flat]])
+	end,
 }
