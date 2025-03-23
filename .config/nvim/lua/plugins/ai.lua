7995d6b11c725dbd399f0dfdb24ecb62a3b86132
Zakk Tapp <zacherytapp@gmail.com>
2025-03-23 10:24:13 -0400

    updated ai stuff

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -3,22 +3,52 @@ return {
 		"github/copilot.vim",
 	},
 	{
-		"olimorris/codecompanion.nvim",
-		config = true,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		enabled = true,
-		cmd = { "CodeCompanion" },
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false,
 		opts = {
-			strategies = {
-				chat = {
-					adapter = "anthropic",
+			provider = "claude",
+			openai = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-3-7-sonnet-20250219",
+				timeout = 30000,
+				temperature = 0,
+				max_tokens = 4096,
+			},
+		},
+		build = "make",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"echasnovski/mini.pick",
+			"nvim-telescope/telescope.nvim",
+			"hrsh7th/nvim-cmp",
+			"ibhagwan/fzf-lua",
+			"nvim-tree/nvim-web-devicons",
+			"zbirenbaum/copilot.lua",
+			{
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						use_absolute_path = true,
+					},
 				},
-				inline = {
-					adapter = "anthropic",
+			},
+			{
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
 				},
+				ft = { "markdown", "Avante" },
 			},
 		},
 	},
